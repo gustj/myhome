c673186558a1006f340f2867b3e81aa208f67a68
gustj <gustj04@naver.com>
2019-05-10 19:00:18 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\강현서\desktop\멋쟁이사자처럼\0404\secondproject\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
